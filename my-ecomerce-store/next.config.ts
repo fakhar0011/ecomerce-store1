056dac71e6ac679a30490ec63926b2d262cd4e5f
Fakhar Islam <fakharislam649@example.com>
2026-06-30 00:04:32 -0700

    changes in next-config.js

--- my-ecomerce-store/next.config.ts
+++ my-ecomerce-store/next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["ik.imagekit.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
